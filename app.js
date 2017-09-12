da7dd075828dec6ad7ff78f595fb06801160b3aa
Max Wolff <maxawolff@hotmail.com>
2017-09-11 17:55:29 -0700

    fixed linter, started working on javascript to add my projects to the resume, added images of the projects

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 'use strict'
+
+function Project(name, url, imageLink, date){
+  this.name = name;
+  this.url = url;
+  this.imageLink = imageLink;
+  this.date = date;
+
+  var appendSpot = document.getElementById('projects');
+
+  this.addToDoc = function(){
+    var projectTitle = document.createElement('p');
+    projectTitle.innerHTML = this.name;
+    appendSpot.appendChild(projectTitle);
+
+    var projectImage = document.createElement('img');
+    projectImage.setAttribute('src', imageLink);
+    appendSpot.appendChild(projectImage);
+  }
+}
+
+var busMall = new Project('Bus Mall', '', '', '');
+var barksNRec = new Project('Barks N Rec', 'https://chelseadole.github.io/barks-and-rec/index.html', 'images/barksNRec.png', '8/30');
+// busMall.addToDoc();
+barksNRec.addToDoc();
