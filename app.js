b418a8aedb8d16984cb05a7251ce99ed652463b5
Max Wolff <maxawolff@hotmail.com>
2017-09-12 10:29:07 -0700

    finished with adding projects and social pages

--- app.js
+++ app.js
@@ -9,17 +9,29 @@ function Project(name, url, imageLink, date){
   var appendSpot = document.getElementById('projects');
 
   this.addToDoc = function(){
-    var projectTitle = document.createElement('p');
+    var projectTitle = document.createElement('h3');
     projectTitle.innerHTML = this.name;
     appendSpot.appendChild(projectTitle);
 
     var projectImage = document.createElement('img');
-    projectImage.setAttribute('src', imageLink);
+    projectImage.setAttribute('src', this.imageLink);
     appendSpot.appendChild(projectImage);
+
+    var projectURL = document.createElement('a');
+    projectURL.innerText = 'link to the ' + this.name + ' project';
+    projectURL.href = this.url;
+    appendSpot.appendChild(projectURL);
+
+    var endDate = document.createElement('p');
+    endDate.innerHTML = 'Completed on ' + this.date;
+    appendSpot.appendChild(endDate);
   }
 }
 
-var busMall = new Project('Bus Mall', '', '', '');
-var barksNRec = new Project('Barks N Rec', 'https://chelseadole.github.io/barks-and-rec/index.html', 'images/barksNRec.png', '8/30');
-// busMall.addToDoc();
+var salmonCookies = new Project('Salmon Cookies', 'https://maxawolff.github.io/cookie-stand/', 'images/salmonCookies.png', '8/18/17');
+var busMall = new Project('Bus Mall', 'https://maxawolff.github.io/bus-mall/', 'images/busMall.png', '8/25/2017');
+var barksNRec = new Project('Barks N Rec', 'https://chelseadole.github.io/barks-and-rec/index.html', 'images/barksNRec.png', '8/31/2017');
+
 barksNRec.addToDoc();
+busMall.addToDoc();
+salmonCookies.addToDoc();
