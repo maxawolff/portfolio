00e7ec085e2d1bd771c2872628a60706d6caa4d7
maxawolff <maxawolff@hotmail.com>
2017-09-26 11:49:27 -0700

    added new files to match mvc style, linked cdn for page.js, added list to index.html to be used for nav later

---
+++ public/scripts/controllers/projectController.js
@@ -0,0 +1,8 @@
+'use strict';
+var app = app || {};
+
+(function(module){
+  const projectController= {};
+
+  module.projectController = projectController;
+})(app);
---
+++ public/scripts/controllers/routes.js
@@ -0,0 +1,10 @@
+'use strict';
+var app = app || {};
+
+page('/', functionTBD);
+page('/about', functionTBD);
+page('/projects', functionTBD);
+page('/skills', functionTBD);
+page('/find', functionTBD);
+
+page();
